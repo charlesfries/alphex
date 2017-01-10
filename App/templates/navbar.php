d474a9e639f916a625c8ba0173c0e912a194c1ff
Charles Fries <charliefries@icloud.com>
2017-01-09 21:47:31 -0800

    Set navbar logo to default right margin

--- App/templates/navbar.php
+++ App/templates/navbar.php
@@ -9,8 +9,7 @@ function resolve_tab($tably)
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand mr-2" href="/"><img src="/img/logo.png" height="22"></a>
-
+  <a class="navbar-brand" href="/"><img src="/img/logo.png" height="22"></a>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item<?=resolve_tab("Trading")?>"><a class="nav-link" href="/trading">Trading</a></li>
